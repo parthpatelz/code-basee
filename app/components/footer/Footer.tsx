c211d46632bb192fcce9c0c4d84ef9838028bcc9
Dumindu Harshana <ruvinduharshana536@gmail.com>
2024-07-24 19:02:29 -0400

    Add files via upload

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link";
+import Container from "../Container";
+import FooterList from "./FooterList";
+import { MdFacebook } from "react-icons/md";
+import {
+  AiFillTwitterCircle,
+  AiFillInstagram,
+  AiFillYoutube,
+} from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <footer className="bg-slate-700 text-slate-200 text-sm mt-16">
+      <Container>
+        <div className="flex flex-col md:flex-row justify-between pt-16 pb-8">
+          
+      
+        </div>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
